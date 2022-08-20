873b78e058cb577f74035dd3fd4026b2636f85e3
MrKubul <jakub.dryka1@gmail.com>
2022-08-20 14:03:35 +0200

    revert static files settings

--- todo_list_django/urls.py
+++ todo_list_django/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('board.urls')),
 
-    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    # re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    # re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
