d7355240f653a11bccef52d8949a38c13a24e631
MrKubul <jakub.dryka1@gmail.com>
2022-08-24 22:00:46 +0200

    working task checkbox

--- board/models.py
+++ board/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
-# class User(models.Model):
-#     username = models.CharField(max_length=200)
-
-#     def __str__(self):
-#         return str(self.username)
-
-
 class ToDoList(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     list_name = models.CharField(max_length=200)
